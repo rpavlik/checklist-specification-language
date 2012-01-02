6c3aa22f7a42f462a3acda684a166f9a9c5bfcc5
Ryan Pavlik <abiryan@ryand.net>
2012-01-01 22:14:45 -0600

    Add license and copyright - bsl 1.0 at least for now

--- checklists.lua
+++ checklists.lua
@@ -1,3 +1,13 @@
+--[[ Checklist Specification Language ]]
+--[[
+            Copyright Ryan Pavlik 2011-2012.
+Distributed under the Boost Software License, Version 1.0.
+    (See accompanying file LICENSE_1_0.txt or copy at
+          http://www.boost.org/LICENSE_1_0.txt)
+]]
+
+
+
 local categories = {}
 local dsl = {}
 
